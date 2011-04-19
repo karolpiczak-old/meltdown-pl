1007d94078943149eb7e8dc07c5adf4383dce60b
jordan <jordan@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2011-04-19 16:53:21 +0000

    creating the skeleton of the updater index template
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@992 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum_modules/updater/views.py
+++ forum_modules/updater/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from base import generate_installation_key
 from settings import SITE_KEY
+from forum.views.admin import admin_tools_page, admin_page
 
+@admin_page
 def updater_index(request):
-    return HttpResponse(str(SITE_KEY))
+    return (
+        'modules/updater/index.html',
+        {
+
+        },
+    )
